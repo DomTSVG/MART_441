3e0a6be12d962ab5e3876b0476fd88ab34a1553c
Dominic <31668455+DomTSVG@users.noreply.github.com>
2021-01-19 15:29:20 -0800

    HW-2 Commit

---
+++ HW-2/readme.md
@@ -0,0 +1,13 @@
+# Homework 2 Readme/Report
+
+## The Good
+
+JavaScript. I suppose it's good to start small, right? After all the reading and such that we did, I'm surprised that everything went as smoothly as it did. I feel fairly and oddly adept at JavaScript, I'll be honest. Writing these console logs is easy, and frankly I accidentally read a little too far into the JavaScript tutorial on the code teachin' site. There was nothing that gave me trouble this week, at all. I can't think of anything that was actually tough about the JavaScript. Although...
+
+## The Bad
+
+My HTML was odd this time around and I have no idea why. I had so much trouble leaving comments on it that it almost broke me. I'm going to have to see if I can do additional fixes.
+
+## The Ugly
+
+See "The Bad", but really nothing terrible happened this week. A great intro to Javascript. Can't wait to learn more.
---
+++ HW-2/script.js
@@ -0,0 +1,27 @@
+// Begin JavaScript goodness.
+
+/* Consider the following:
+This was made by: Dominic Rosatti
+Date created: January 19th, 2021 */
+
+// My Favorite Websites Section
+console.log("My Favorite Websites:")
+console.log("1. YouTube") // Video watching/hosting.
+console.log("2. Twitch") // I livestream D&D and stuff.
+console.log("3. The Kingdom of Loathing") // My favorite browser based game.
+
+console.log(" ") // Space to seperate log lines.
+
+// My Favorite Games Section
+console.log("My Favorite Games:")
+console.log("1. Pokemon Mystery Dungeon: Explorers of Sky") // My number 1. Easy.
+console.log("2. Celeste") // My number 2, a fantastic platformer.
+console.log("3. Call of Duty: Modern Warfare 2") // My number 3 and favorite FPS.
+
+console.log(" ") // Space to seperate log lines.
+
+// My Favorite Artists Section
+console.log("My Favorite Artists")
+console.log("1. Adam Young") // My favorite musical artist.
+console.log("2. Katheryn Lasky") // My favorite writer, or at the very least she wrote my favorite book series.
+console.log("3. Ken Sugimori") // My favorite artist.
